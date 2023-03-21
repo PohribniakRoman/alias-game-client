2ac67fcc6b1e598311d34cf7012fa263412285f3
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-03-21 14:15:11 +0200

    add socket room

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {socket} from "../../ws";
 import {Navigate, useParams} from "react-router-dom";
 import {validate} from "uuid";
@@ -6,10 +6,22 @@ import {useSelector} from "react-redux";
 const Game = props => {
     const profile = useSelector(state=>state.profile)
     const {gameid} = useParams();
-    if(!validate(gameid)){
+    const isGameAlive = !validate(gameid);
+    useEffect(()=>{
+
+        socket.emit("ENTER",{user:{id:profile.id,name:profile.data.username},gameId:gameid})
+
+        return ()=>{
+            socket.emit("LEAVE",{user:{id:profile.id,name:profile.data.username},gameId:gameid})
+        }
+    },[])
+
+
+    if(isGameAlive){
         return <Navigate to="/lobbies"/>
     }
-    socket.emit("ENTER",{user:{id:profile.id,name:profile.data.username},gameId:gameid})
+
+
 
     return (
         <div>
--- src/components/game/Lobbies.jsx
+++ src/components/game/Lobbies.jsx
@@ -1,7 +1,14 @@
 import {Navigation} from "../home/Navigation";
+import {socket} from "../../ws";
 
 export const Lobbies = () => {
+    socket.emit("GET_LOBBIES");
+    socket.on("SHARE_LOBBIES",data=>{
+        console.log(data)
+    })
     return(<>
         <Navigation/>
+
+
     </>)
 }

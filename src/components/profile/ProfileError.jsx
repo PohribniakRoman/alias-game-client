4151ddea82c9c62a241ddcda79bcd5d5d6338dd2
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-04-06 18:56:28 +0300

    deploy

--- src/components/profile/ProfileError.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const ProfileError = props => {
-    return (
-        <div>
-
-        </div>
-    );
-};
-export default ProfileError;
--- src/components/reducers/combineReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { notifications } from "./notifications";
-import { profile } from "./profile";
-
-
-export const combineReducer = combineReducers({
-        notifications,
-        profile,
-    })

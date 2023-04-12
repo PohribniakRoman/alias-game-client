9b6f3a070008c8cb8a8f6b7badfa753e76b00717
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-04-12 21:46:25 +0300

    add styles

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,17 @@
-import {Navigation} from "./Navigation";
+import { Typography } from "@mui/material";
+import { Navigation } from "./Navigation";
 
 export const Home = () => {
-    return <section className="home">
-            <Navigation/>
-            <div className="home__rules">
-                    <h1 className="home__rules--title">Rules</h1>
-            </div>
-    </section>
+        return <section className="home">
+                <Navigation />
+                <div className="home__rules">
+                        <Typography className="home__rules--title" variant="h2">Rules</Typography>
+                        <div className="home__rules--description">
+                                <Typography variant="h4">Object of the Game</Typography>
+                                <p>Explain words using other words, synonyms or opposites! 
+                                Try to get your partner or team to correctly guess as many words
+                                as possible before the sand in the timer runs out.</p>
+                        </div>
+                </div>
+        </section>
 }
--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {Navigation} from "../home/Navigation";
 import {ENDPOINTS} from "../../ENDPOINTS";
 import {Link} from "react-router-dom";
+import { ListItem, ListItemText } from '@mui/material';
 
 const Users = props => {
     const [userList,updateUserList] = useState([]);
@@ -13,12 +14,10 @@ const Users = props => {
     },[])
     return (<>
         <Navigation/>
-        <div className="profile__friends" style={{position:"absolute",top:"50%",left:"50%",transform:"translate(-50%,-50%)"}}>
             <h1 className="profile__friends--title">All users</h1>
             {userList.map(user=>{
-                return <Link key={user.id} to={`/profile/${user.id}`}><div className="profile__friends--list-item">{user.username}</div></Link>;
+                return <Link key={user.id} to={`/profile/${user.id}`}><ListItem button><ListItemText>{user.username}</ListItemText></ListItem></Link>;
             })}
-        </div>
     </>);
 };
 export default Users;
